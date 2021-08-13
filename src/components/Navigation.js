a7739785a9d8bc7bc144dd799d0917e93b91eb22
LucileGombert <lucile.gombert.lg@gmail.com>
2021-08-13 16:08:06 +0200

    path img

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-import imageme from './me.JPG';
+// import imageme from './me.JPG';
 import imageme3 from './images/me.JPG';
 // import imageme4 from '/images/me.JPG';
 const Navigation = () => {
@@ -15,7 +15,7 @@ const Navigation = () => {
                     <img src={window.location.origin + './images/me.JPG'} alt="Profil4"/>
                     {/* <img src={window.location.origin + '/images/me.JPG'} alt="Profil5"/> */}
                     <img src={window.location.origin + 'images/me.JPG'} alt="Profil6"/>
-                    <img src={imageme} alt="Profil7"/>
+                    {/* <img src={imageme} alt="Profil7"/> */}
                     <img src={imageme3} alt="Profil9"/>
                     {/* <img src={imageme4} alt="Profil10"/> */}
                     <h2>Lucile Gombert</h2>
