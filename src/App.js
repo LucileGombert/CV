f537440f8286e3544c2637678b5976242e2aeb22
LucileGombert <lucile.gombert.lg@gmail.com>
2021-08-13 16:49:36 +0200

    path img

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
     <div>
       <BrowserRouter>
         <Switch>
-          <Route path='/CV/' exact component={Home} />
+          <Route path='/' exact component={Home} />
           <Route path='/parcours' component={Career} />
           <Route path='/competences' component={Skills} />
           <Route path='/portfolio' component={Portfolio} />
