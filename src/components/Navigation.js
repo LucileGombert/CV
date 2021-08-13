8d661ca000b7e5911176bfb489222bcaf4be263b
LucileGombert <lucile.gombert.lg@gmail.com>
2021-08-13 14:31:19 +0200

    deploy cv

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import ProfileImage from "./media/me.JPG";
+import ProfileImage from "../../media/me.JPG";
 
 const Navigation = () => {
     return (
